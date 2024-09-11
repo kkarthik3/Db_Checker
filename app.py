6c849d749fbb087a0bfb72fa42c6e6d926e1386b
karthikeyan <karthikeyan@ispgweb.com>
2024-09-11 12:33:55 +0530

    Bug sorted

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain.schema import (
 from verifylead import verify_lead,extract_json_from_string
 import json
 
+st.set_page_config("Automotive Sales and Processes", "🚗","wide")
 st.title("Automotive Sales and Processes")
 
 llm = ChatGroq(
@@ -28,16 +29,18 @@ Return the information in a json format.
 
 Context:
 The text may include personal details mixed with other information. The required fields could appear in different formats.
-input may be single value pleas note that
+input may be single value please note that
 
 Negative Prompting:
 Do not extract unrelated data like addresses or social media handles.
 Exclude incomplete emails or invalid phone numbers with empty strings "".
 Avoid generic placeholders.
+Dont Include Values that are not present in the given user input
+
 
 Example:
 Input:
-Contact karthick via karthick@example.com or at +919876543210. His civil ID is 123456789012.
+karthick,karthick@example.com ,+919876543210,123456789012.
 
 Output:
 {"Name": "karthick", "Email": "karthick@example.com", "Phone Number": "+919876543210", "Civil ID Number": "123456789012"}
@@ -67,6 +70,8 @@ for message in st.session_state.messages:
         with st.chat_message(role):
             st.markdown(message.content)
 
+verify_lead_output = None
+
 if prompt:= st.chat_input("Ask questions about the automotive sales and processes"):
     st.session_state.messages.append(HumanMessage(content=prompt))
 
@@ -87,16 +92,21 @@ if prompt:= st.chat_input("Ask questions about the automotive sales and processe
         try:
             json_resp = json.loads(json_resp_str)
             verify_lead_output  = verify_lead(
-                            name=json_resp['Name'].strip() or None,
-                            email=json_resp['Email'].strip() or None,
-                            phone=json_resp['Phone Number'].strip() or None,
-                            civil_id=json_resp['Civil ID Number'].strip() or None
+                            name=json_resp.get('Name',None),
+                            email=json_resp.get('Email',None),
+                            phone=json_resp.get('Phone Number',None),
+                            civil_id=json_resp.get('Civil ID Number',None)
                         )
         except Exception as e:
             verify_lead_output = json_resp_str
 
         st.markdown(json_resp_str)
         print(verify_lead_output)
-        with st.sidebar:
-            with st.container(border=True):
-                st.write(verify_lead_output)
+
+with st.sidebar:
+    st.title("Output Tool Message To The Primary Agent")
+    with st.container(border=True):
+        if verify_lead_output:
+            st.markdown(verify_lead_output)
+        else:
+            st.markdown("Please Give input To Proceed")
